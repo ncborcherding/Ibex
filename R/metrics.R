055c2d54f9874405bcb1032ccad00ecd11180b5d
theHumanBorch <ncborch@gmail.com>
2022-08-05 19:35:54 -0500

    Converting to BCR
    Changing the nomenclature for BCR and naming conventions

--- R/metrics.R
+++ R/metrics.R
@@ -1,10 +1,10 @@
 #Transforming the CDR3 sequence into vectors
 #' @importFrom keras load_model_hdf5
 #' @importFrom reticulate array_reshape
-aaProperty <- function(TCR, 
+aaProperty <- function(BCR, 
                        AA.properties = AA.properties) { 
   return <- list() ### Need to add reference data
-  reference <- Trex.Data[[1]] #AA properties
+  reference <- Ibex.Data[[1]] #AA properties
   col.ref <- grep(tolower(paste(AA.properties, collapse = "|")), colnames(reference))
   length <- NULL
   if (AA.properties == " both") {
@@ -12,13 +12,13 @@ aaProperty <- function(TCR,
   } else {
     column.ref <- unique(sort(col.ref))
   }
-  chain <- names(TCR)
-  for (i in seq_along(TCR)) {
-    membership <- TCR[[i]]
+  chain <- names(BCR)
+  for (i in seq_along(BCR)) {
+    membership <- BCR[[i]]
     names <- membership$barcode
     array.reshape <- NULL
     aa.model <- quiet(aa.model.loader(chain[[i]], AA.properties))
-    range <- aa.range.loader(chain[[i]], AA.properties, Trex.Data) 
+    range <- aa.range.loader(chain[[i]], AA.properties, Ibex.Data) 
     local.min <- range[[1]]
     local.max <- range[[2]]
     }
@@ -27,14 +27,14 @@ aaProperty <- function(TCR,
     for (n in seq_len(length(cells))) {
       tmp.CDR <- membership[membership$barcode == cells[n],]$cdr3_aa
       refer <- unlist(strsplit(tmp.CDR, ""))
-      refer <- c(refer, rep(NA, 60 - length(refer)))
+      refer <- c(refer, rep(NA, 70 - length(refer)))
       if(AA.properties == "OHE") {
         int <- one.hot.organizer(refer)
-        array.reshape.tmp <- array_reshape(int, 1260)
+        array.reshape.tmp <- array_reshape(int, 1470)
       }else {
         int <- reference[match(refer, reference$aa),c(1,col.ref)]
         int <- as.matrix(int[,-1])
-        array.reshape.tmp <- array_reshape(int, length(col.ref)*60)
+        array.reshape.tmp <- array_reshape(int, length(col.ref)*70)
       }
       score.tmp <- auto.embedder(array.reshape.tmp, aa.model, local.max, local.min, AA.properties)
       score <- rbind(score, score.tmp)
@@ -43,7 +43,7 @@ aaProperty <- function(TCR,
     barcodes <- score[,1]
     score <- score[,-1]
     rownames(score) <- barcodes
-    colnames(score) <- paste0("Trex_", seq_len(ncol(score)))
+    colnames(score) <- paste0("Ibex_", seq_len(ncol(score)))
     return(score)
 }
 
