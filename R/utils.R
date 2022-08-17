9bf4e66689fba83becaa5967a01462c4b4aa12b9
theHumanBorch <ncborch@gmail.com>
2022-08-17 12:42:41 -0500

    Update utils.R
    getBCR pulls heavy then light

--- R/utils.R
+++ R/utils.R
@@ -31,15 +31,15 @@ getBCR <- function(sc, chains) {
                     str_split(meta[,"CTaa"], "_", simplify = TRUE), 
                     str_split(meta[,"CTgene"], "_", simplify = TRUE))
   if (length(chains) == 1 && chains != "both") {
-    if (chains %in% c("TRA", "TRD")) { #here
+    if (chains %in% c("heavy")) { #here
       pos <- list(c(2,4))
-    } else if (chains %in% c("TRB", "TRG")) { #here
+    } else if (chains %in% c("light")) { #here
       pos <- list(c(3,5))
     }
   } else {
     pos <- list(one = c(2,4), two = c(3,5))
-    ch.1 <- grep("TRB|TRA",sc[[]]$CTgene[1]) #here
-    chains <- c("TRA", "TRB") #here
+    ch.1 <- grep("IGH|IGL",sc[[]]$CTgene[1]) #here
+    chains <- c("heavy", "light") #here
   }
   BCR <- NULL
   for (i in seq_along(pos)) {
