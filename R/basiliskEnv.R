d5e90713f85b7d5eac0fd0c4514aa71241426ec0
theHumanBorch <ncborch@gmail.com>
2025-07-31 12:21:32 -0500

    fixing testthat failures

--- R/basiliskEnv.R
+++ R/basiliskEnv.R
@@ -1,20 +1,12 @@
 #' @import basilisk
-.get_ibex_env <- local({
-  env <- NULL
-  function() {
-    if (is.null(env)) {
-      env <<- basilisk::BasiliskEnvironment(
-        envname = "IbexEnv",
-        pkgname = "Ibex",
-        packages = c(
-          "python=3.9",
-          "keras=3.6.*",
-          "tensorflow=2.18.*",
-          "h5py=3.13",
-          "numpy=1.26"
-        )
-      )
-    }
-    env
-  }
-})
+IbexEnv <- BasiliskEnvironment(
+  envname = "IbexEnv",
+  pkgname = "Ibex",
+  packages = c(
+    "python=3.9",
+    "keras=3.6.*",            
+    "tensorflow=2.18.*",      
+    "h5py=3.13",
+    "numpy=1.26"
+  )
+)
